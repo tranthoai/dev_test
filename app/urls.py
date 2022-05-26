efeaeacc357d5f642986e774c5aaa892d8066f5a
Thoai Tran <tranthoaipy@gmail.com>
2022-05-27 02:56:16 +0700

    Begin Implement function: register, login, logout

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ app_name = "app"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("home", views.home, name="home"),
-    path("register", views.register_request, name="register")
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name="logout")
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
 from .forms import NewUserForm
+
+
+def home(request):
+    return render(
+        request=request,
+        template_name="home.html",
+    )
 
 
 def register_request(request):
@@ -31,12 +39,45 @@ def register_request(request):
     return render(
         request=request,
         template_name="register.html",
-        context={"register_form": form}
+        context={
+            "register_form": form
+        }
     )
 
 
-def home(request):
-    return HttpResponse(
-        "HomePage",
-        content_type="text/plain"
+def login_request(request):
+    if request.method == "POST":
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("app:home")
+            else:
+                messages.error(request, "Invalid username or password.")
+
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    form = AuthenticationForm()
+
+    return render(
+        request=request,
+        template_name="login.html",
+        context={
+            "login_form": form
+        }
     )
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("app:home")
